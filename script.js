2a833add1347fa62cd11538db5c6fba338637ba4
AngelLilz <a.ardiz76@gmail.com>
2024-08-21 14:12:59 +0800

    Added moment.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let currentTime = new Date();
-
-let currentYear = currentTime.getFullYear();
-let currentDay = "Thursday";
-let currentMonth = "August";
-let currentDate = "15th";
-
-let tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
